e609d4d9d70543172eef71b2a2df676898e385b6
matinator <50795539+matinator@users.noreply.github.com>
2023-06-01 14:50:57 +0200

    Open simple window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-use crosswebx::hello;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
 
 fn main() {
-    let s = hello();
-    println!("{s}");
+    let event_loop = EventLoop::new();
+    let _ = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                control_flow.set_exit();
+            }
+            _ => {}
+        }
+    });
 }
